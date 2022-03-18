a9e7c7bf944e20363f47258233418dbc743684b5
djordje997 <86196338+djordje997@users.noreply.github.com>
2022-03-18 14:42:20 +0100

    return all anagrams of given file functionlaity

--- lib/anagram_finder.rb
+++ lib/anagram_finder.rb
@@ -1,32 +1,23 @@
-
 require 'pry'
+
 class AnagramFinder
   def initialize(dictionary)
     @dictionary = dictionary
-    @file = @dictionary[0]
-    @signatures = nil
   end
 
   def find_anagrams
-    signatures
-    if @dictionary.size == 1 
-      stored_anagrams = find_all_anagrams(@signatures)
-    else
-      words_to_find = @dictionary.drop(1)
-      stored_anagrams = find_anagrams_for_selected_words(words_to_find)
-    end
-    stored_anagrams
+    find_all_anagrams(signatures)
   end
 
   private
 
-  def find_anagrams_for_selected_words(words)
-    anagrams = []
-    words.each do |word|
-      anagrams << anagrams_for_word(word)
-    end
-    anagrams
-  end
+  # def find_anagrams_for_selected_words(words)
+  #   anagrams = []
+  #   words.each do |word|
+  #     anagrams << anagrams_for_word(word)
+  #   end
+  #   anagrams
+  # end
 
   def find_all_anagrams(all_signatures)
     all_anagrams = []
@@ -47,24 +38,23 @@ class AnagramFinder
     words_from_file = read_from(@dictionary[0])
     words_from_file.each do |line|
       word = line.chomp
-      signature = sort_word(word)
+      signature = word_signature(word)
       all_signatures_from_file[signature] << word
     end
     all_signatures_from_file
   end
 
-  def anagrams_for_word(word)
-    user_signature = sort_word(word)
-    @signatures[user_signature]
-  end
+  # def anagrams_for_word(word)
+  #   user_signature = sort_word(word)
+  #   @signatures[user_signature]
+  # end
 
   def read_from(file_name)
     File.readlines(file_name)
   end
 
-  def sort_word(word)
-    sorted_word = word.chars.sort.join
-    sorted_word
+  def word_signature(word)
+    word.chars.sort.join
   end
 end
 
