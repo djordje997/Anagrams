d35b371de4fbc3619d49e7923f177bfc297afdb1
djordje997 <86196338+djordje997@users.noreply.github.com>
2022-04-07 14:22:12 +0200

    deleted tests

--- spec/anagrams_spec.rb
+++ spec/anagrams_spec.rb
@@ -63,42 +63,4 @@ describe "AnagramFinder" do
     end
   end
 end
-
-
-#     it "returns an array of words if first argument (ARGV[0]) is path to file and rest are words to be found" do
-#       anagrams = AnagramFinder.new(["spec/files/wordlist.txt"])
-#       words_to_find = ["enlist","sort"]
-#       anagram_value = anagrams.find_anagrams(words_to_find)
-#       expect(anagram_value).to be_an_instance_of(Array)
-#     end
-#     it "returns an array of words when prompt takes path to file and words to be found(string)" do
-#       anagrams = AnagramFinder.new("spec/files/wordlist.txt")
-#       words_to_find = ["enlist"]
-#       anagram_value = anagrams.find_anagrams(words_to_find)
-#       expect(anagram_value).to be_an_instance_of(Array)
-#     end
-#   end
-
-#   context "Returns correct values" do
-#     it "returns all anagrams in file if first argument (ARGV[0]) is path to file" do
-#     anagrams = AnagramFinder.new(["spec/files/word.txt"])
-#     words_to_find = []
-#     anagram_value = anagrams.find_anagrams(words_to_find)
-#     expect(anagram_value).to eq([["enlist", "listen", "lisent", "silent"], ["sort", "rots", "tors"]])
-#     end
-#     it "returns only anagrams of selected if first argument (ARGV[0]) is path to file and rest are words to be found" do
-#       anagrams = AnagramFinder.new(["spec/files/word.txt"])
-#       words_to_find = ["enlist"]
-#       anagram_value = anagrams.find_anagrams(words_to_find)
-#       expect(anagram_value).to eq([["enlist", "listen", "lisent", "silent"]])
-#     end
-#     it "returns an array of words when prompt takes path to file and words to be found (string)" do
-#       anagrams = AnagramFinder.new("spec/files/word.txt")
-#       words_to_find = ["enlist"]
-#       anagram_value = anagrams.find_anagrams(words_to_find)
-#       expect(anagram_value).to eq([["enlist", "listen", "lisent", "silent"]])
-#     end
-#   end
-# end
-
   
