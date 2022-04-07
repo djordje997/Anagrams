bfd94e3222c73a655a2dc5c8c42f1fda101b07cd
djordje997 <86196338+djordje997@users.noreply.github.com>
2022-04-07 14:46:13 +0200

    added let in tests

--- spec/anagrams_spec.rb
+++ spec/anagrams_spec.rb
@@ -2,17 +2,18 @@ require_relative '../lib/anagram_finder.rb'
 
 describe "AnagramFinder" do
   context "find_anagrams() returns all anagrams" do
-    context "Always return array if words_to_find.empty?" do
+    let(:anagrams_wordlist_txt) { AnagramFinder.new("spec/files/wordlist.txt") }
+
+    context "Always return array if words to find is empty" do
       it "returns an array if array is passed " do
-      anagrams = AnagramFinder.new(["enlist"])
-      words_to_find = []
-      anagram_value = anagrams.find_anagrams(words_to_find)
-      expect(anagram_value).to be_an_instance_of(Array)
-      end
-      it "returns an array if path to file is passed" do
-        anagrams = AnagramFinder.new("spec/files/wordlist.txt")
+        anagrams = AnagramFinder.new(["enlist"])
         words_to_find = []
         anagram_value = anagrams.find_anagrams(words_to_find)
+        expect(anagram_value).to be_an_instance_of(Array)
+      end
+      it "returns an array if path to file is passed" do
+        words_to_find = []
+        anagram_value = anagrams_wordlist_txt.find_anagrams(words_to_find)
         expect(anagram_value).to be_an_instance_of(Array)
       end
       it "returns an array if string is passed" do
@@ -24,7 +25,7 @@ describe "AnagramFinder" do
     end
 
     context "Return correct values" do
-    it "AnagramFinder.new([\"enlist\", \"listen\", \"sort\", \"rots\", \"no\", \"yes\"])" do
+      it "AnagramFinder.new([\"enlist\", \"listen\", \"sort\", \"rots\", \"no\", \"yes\"])" do
         anagrams = AnagramFinder.new(["enlist", "listen", "sort", "rots", "no", "yes"])
         words_to_find = []
         anagram_value = anagrams.find_anagrams(words_to_find)
@@ -43,23 +44,22 @@ describe "AnagramFinder" do
         expect(anagram_value).to eq([["enlist", "listen", "lisent", "silent"], ["sort", "rots", "tors"]])
       end
     end
-  end
-  context "find_anagrams() return anagrams for selected words" do
-    context "Always returns array if words_to_find is not empty" do
-      it "find_anagrams([enlist, sort])" do
-        anagrams = AnagramFinder.new("spec/files/wordlist.txt")
-        words_to_find = ['enlist', 'sort']
-        anagram_value = anagrams.find_anagrams(words_to_find)
-        expect(anagram_value).to be_an_instance_of(Array)
+
+    context "find_anagrams() return anagrams for selected words" do
+      context "Always returns array if words_to_find is not empty" do
+        it "find_anagrams([enlist, sort])" do
+          words_to_find = ['enlist', 'sort']
+          anagram_value = anagrams_wordlist_txt.find_anagrams(words_to_find)
+          expect(anagram_value).to be_an_instance_of(Array)
+          end
+      end
+      context "Returns correct values" do
+        it "find_anagrams([enlist, sort])" do
+          words_to_find = ['enlist', 'sort']
+          anagram_value = anagrams_wordlist_txt.find_anagrams(words_to_find)
+          expect(anagram_value).to eq([["elints", "enlist", "inlets", "listen", "silent", "tinsel"], ["orts", "rots", "sort", "stor", "tors"]])
         end
-    end
-    context "Returns correct values" do
-      it "find_anagrams([enlist, sort])" do
-        anagrams = AnagramFinder.new("spec/files/wordlist.txt")
-        words_to_find = ['enlist', 'sort']
-        anagram_value = anagrams.find_anagrams(words_to_find)
-        expect(anagram_value).to eq([["elints", "enlist", "inlets", "listen", "silent", "tinsel"], ["orts", "rots", "sort", "stor", "tors"]])
-        end
+      end
     end
   end
 end
